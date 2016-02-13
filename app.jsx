812d52d8b4a1343db1e3b1bb72bad06c5645325d
Ismael Slimane Zubillaga <isma.bled777@gmail.com>
2016-02-13 19:24:37 +0100

    Implemented ajax call to get the words from 'vocabulary.json'

--- app.jsx
+++ app.jsx
@@ -1,9 +1,41 @@
 var VocabularyApp = React.createClass({
+  loadWordsFromServer: function() {
+    $.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+  // // "props" are immutable, to implement interactions we add
+	// // "state".
+	getInitialState: function() {
+	    return {data: []};
+	},
+	// Get the json file
+	componentDidMount: function() {
+		this.loadWordsFromServer();
+	},
   render: function(){
+    var wordsNodes = this.state.data.map(function(wordNode){
+			return (
+				<div>
+					{wordNode.ar_word}: {wordNode.es_word}
+				</div>
+			);
+		});
     return(
       <div className="app_box">
         <span className="ar_word">نَحِيفْ</span>:
         <input type="text" placeholder="Translation..." ref="translation" />
+        <div>
+          {wordsNodes}
+        </div>
       </div>
     );
   }
@@ -14,7 +46,7 @@ var VocabularyBox = React.createClass({
     return(
       <div>
         <h1>Translate the next word:</h1>
-        <VocabularyApp/>
+        <VocabularyApp url="vocabulary.json"/>
       </div>
     );
   }
